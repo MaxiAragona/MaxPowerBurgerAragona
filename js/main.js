76b1ca9527397e0308faca0a3de35f4de10890e8
maxiaragona <aragona_9@hotmail.com>
2023-11-20 17:05:56 -0300

    proyecto listo

--- js/main.js
+++ js/main.js
@@ -1,83 +1,45 @@
-//
-// Base de datos estatica de hamburguesa
-//
-const hamburguesas = [
-    {   
-        id: 1,
-        titulo: "AMERICAN DOBLE",
-        descripcion: "Medallon x2 Cheddar Fetas x4 Tomate Lechuga Cebolla y Thousand Island.",
-        imagen: "img/american-doble.jpeg",
-        precio: 4550,
-    },
-    {   
-        id: 2,
-        titulo: "MAX POWER",
-        descripcion: "Medallon x2 Cheddar Fetas x4 Bacon Cebolla Crispy salsa power doble.",
-        imagen: "img/maxpower.jpeg",
-        precio: 4800,
-    },
-    {   
-        id: 3,
-        titulo: "ONION",
-        descripcion: "Medallon x2 Cheddar x4 Cebolla Grilled en pan de papa con salsa po",
-        imagen: "img/onion.jpeg",
-        precio: 4400,
-    },
-    {   
-        id: 4,
-        titulo: "CALIFORNIANA",
-        descripcion: "Medallón, cheddar x2, lechuga, tomate, cebolla, salsa picante en pan brioche.",
-        imagen: "img/american-simple.jpeg",
-        precio: 4000,
-    },
-    {   
-        id: 5,
-        titulo: "ROYAL POWER",
-        descripcion: "Medallon x2 Cheddar Fetas x4 Cebolla Ketchup mayonesa en pan de papa.",
-        imagen: "img/royal.jpeg",
-        precio: 4300,
-    },
-    {   
-        id: 6,
-        titulo: "TRIPLE CHEES",
-        descripcion: "Medallon x3 Cheddar Fetas x6 en un pan brioche con papas fritas y salsa power.",
-        imagen: "img/triplecheese.jpeg",
-        precio: 5000,
-    },
-];
+let hamburguesas = [];
 
-//
-// funciones para el manejo del carrito
-//
+// Función para obtener de forma asíncrona los datos de hamburguesas desde JSON
+const obtenerHamburguesasDesdeJSON = async () => {
+    try {
+        const respuesta = await fetch("data.json");
+        const datos = await respuesta.json();
+        hamburguesas = datos; 
+        dibujarHamburguesas();
+        actualizar();
+    } catch (error) {
+        console.error('Error al obtener datos de hamburguesas:', error);
+    }
+};
+
+// Funciones para el manejo del carrito
 const obtenerCarrito = () => {
     let carrito = localStorage.getItem("carrito");
     if (carrito) {
         carrito = JSON.parse(carrito);
     }
     return carrito || [];
-}
+};
 
 const guardarCarrito = (carrito) => {
     localStorage.setItem("carrito", JSON.stringify(carrito));
     actualizar();
-}
+};
 
 const vaciarCarrito = () => {
     localStorage.removeItem("carrito");
     actualizar();
-}
+};
 
 const agregarAlCarrito = (id) => {
-    //buscamos la hamburguesa en nuestra base de hamburguesa
     const hamburguesa = hamburguesas.find(h => h.id == id);
 
     if (hamburguesa) {
         const carrito = obtenerCarrito();
-        //verificamos si ya existe la hamburguesa en el carrito
         const hamburguesaEnCarrito = carrito.find(h => h.id == id);
 
         if (!hamburguesaEnCarrito) {
-            //como no existe la hamburguesa en el carrito , la agregamos.
             carrito.push({
                 id,
                 hamburguesa,
@@ -85,7 +47,6 @@ const agregarAlCarrito = (id) => {
             });
 
         } else {
-            //la hamburguesa ya existe en el carrito tenemos que modificar la cantidad.
             hamburguesaEnCarrito.cantidad += 1;
         }
 
@@ -99,33 +60,31 @@ const agregarAlCarrito = (id) => {
             timer: 1500
         });
     }
-}
+};
 
 const eliminarDelCarrito = (id) => {
-    //buscamos la hamburguesa en nuestra base de hamburguesa
     const hamburguesa = hamburguesas.find(h => h.id == id);
 
     if (hamburguesa) {
         const carrito = obtenerCarrito();
-        //filtramos del carrito las hamburguesas que no quiero eliminar
         const carritoFiltrado = carrito.filter(h => h.id != id);
 
         guardarCarrito(carritoFiltrado);
         Swal.close();
         verCarrito();
     }
-}
+};
 
 const verCarrito = () => {
     const carrito = obtenerCarrito();
 
     if (!carrito.length) {
         return Swal.fire({
-            title: "todavia no agregaste ninguna hamburguesa",
+            title: "todavía no agregaste ninguna hamburguesa",
             showCloseButton: false,
             focusConfirm: true,
             confirmButtonText: "aceptar",
-          });
+        });
     }
 
     const html = carrito
@@ -150,7 +109,7 @@ const verCarrito = () => {
         html: `
             ${html}
             <div class="d-flex justify-content-end align-items-center mt-3">
-               <b> total $ ${carrito.reduce((a, h) => a + (h.hamburguesa.precio* h.cantidad), 0)} </b>
+               <b> total $ ${carrito.reduce((a, h) => a + (h.hamburguesa.precio * h.cantidad), 0)} </b>
             </div>
             <div class="d-flex justify-content-center align-items-center mt-3">
                 <a href="#" onclick="vaciarCarrito(); Swal.close()">vaciar carrito</a>
@@ -165,14 +124,14 @@ const verCarrito = () => {
             confirmButton: "btn btn-success",
             cancelButton: "btn btn-danger"
         },
-      })
-      .then((result) => {
+    })
+    .then((result) => {
         if (result.isConfirmed) {
-            Swal.fire("Felicitaciones! compra realizada correctamente", "", "success");
+            Swal.fire("¡Felicitaciones! Compra realizada correctamente", "", "success");
             vaciarCarrito();
         }
-      });
-}
+    });
+};
 
 const actualizar = () => {
     const cantidadEnCarrito = document.getElementById("cantidadEnCarrito");
@@ -180,22 +139,41 @@ const actualizar = () => {
     const carrito = obtenerCarrito();
 
     if (!carrito.length) {
-        //si no hay elementos en el carrito ocultamos el indicador de cantidad
         cantidadEnCarrito.classList.remove("d-inline");
         cantidadEnCarrito.classList.add("d-none");
-        cantidadDeHamburguesasEnCarrito.innerHTML = "0"
+        cantidadDeHamburguesasEnCarrito.innerHTML = "0";
     } else {
-        //si hay elementos mostramos y actualizamos el indicador de cantidad
         cantidadEnCarrito.classList.add("d-inline");
         cantidadEnCarrito.classList.remove("d-none");
         cantidadDeHamburguesasEnCarrito.innerHTML = `${carrito.reduce((a, h) => a + h.cantidad, 0)}`;
     }
-}
+};
 
-//iniciando el script
+const dibujarHamburguesas = () => {
+    const contenedorDeHamburguesas = document.getElementById("contenedorDeHamburguesas");
+
+    hamburguesas.forEach(hamburguesa => {
+        // Appendeamos las hamburguesas en el elemento contenedor
+        contenedorDeHamburguesas.innerHTML += `
+            <article data-aos="flip-left" class="col">
+                <div class="card shadow-sm ">
+                    <img src="${hamburguesa.imagen}" alt="foto american doble">
+                    <div class="card-body containerFondo">
+                        <h3 class="text-center subTitulos">${hamburguesa.titulo}</h3>
+                        <p class="card-text">${hamburguesa.descripcion}</p>
+                        <h5 class="card-text"><b>$ ${hamburguesa.precio}</b></h5>
+                        <div class="d-flex justify-content-center align-items-center">
+                            <button onclick="agregarAlCarrito(${hamburguesa.id})" class="btnAgregar btn btn-dark text-uppercase">agregar al carrito</button>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        `;
+    });
+
+};
+
+// Llama a la función para obtener los datos de hamburguesas cuando la ventana carga
 window.addEventListener('load', () => {
-    actualizar();
+    obtenerHamburguesasDesdeJSON();
 });
-
-
-
